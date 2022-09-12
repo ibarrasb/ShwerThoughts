4e1bbaf782b611e76a3c232fb6cc22fe1b40a1b2
Eddie Ibarra <eddieibarra43@gmail.com>
2022-09-12 12:02:42 -0500

    base

---
+++ ShwerThoughts/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ShwerThoughts
+//
+//  Created by eduardo.ibarra on 9/12/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ShwerThoughts/ShwerThoughtsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShwerThoughtsApp.swift
+//  ShwerThoughts
+//
+//  Created by eduardo.ibarra on 9/12/22.
+//
+
+import SwiftUI
+
+@main
+struct ShwerThoughtsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
